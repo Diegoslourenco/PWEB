2a253765022bf2aae75f7297a964f29be4df8198
Diegoslourenco <diego.lourenco15@gmail.com>
2022-11-26 19:05:18 -0300

    atividade 12

---
+++ Atividade10/CriaObjeto.js
@@ -0,0 +1,14 @@
+        //1ª forma
+        var aluno1 = "Diego da Silva Lourenço";
+        aluno1.ra = "0030482011037";
+        
+        //2ª forma
+        var aluno2 = {};
+        aluno2.nome = "Diego da Silva Lourenço";
+        aluno2.ra = "0030482011037";
+        
+        //3ª forma
+        var aluno3 = {
+            nome: "Diego da Silva Lourenço",
+            ra: "0030482011037",
+        };
